3c1efa584222a1807eb9c4af39976cbd68e70a9f
McLemoreT <mclemoret0@students.rowan.edu>
2021-10-29 18:42:53 -0400

    Added CumSum block

--- src/main/scala/Passthrough.scala
+++ src/main/scala/Passthrough.scala
@@ -2,22 +2,28 @@ import chisel3._
 
 
 // Chisel Code, but pass in a parameter to set widths of ports
-class PassthroughGenerator(width: Int) extends Module { 
+class CumSum(width: Int) extends Module { 
   val io = IO(new Bundle {
-    val in = Input(UInt(width.W));
-    val out = Output(UInt(width.W));
-    val meme = Output(UInt(width.W));
+	val MA_1 = Input(UInt(width.W));
+	val MA_2 = Input(UInt(width.W));
+	val MA_3 = Input(UInt(width.W));
+
+	val MB_1 = Input(UInt(width.W));
+	val MB_2 = Input(UInt(width.W));
+	val MB_3 = Input(UInt(width.W));
+
+	
+	val out = Output(UInt(width.W));
   });
   
-  val memeReg = RegInit(420.U(width.W));
+ val OutReg = RegInit(0.U(width.W));
   
-  io.out := io.in + 1.U;
-  memeReg := io.in + 420.U;
-  io.meme := memeReg;
+  OutReg := (io.MA_1 * io.MB_1) + (io.MA_2 * io.MB_2) + (io.MA_3 * io.MB_3);
+  io.out := OutReg;
 }
 
 
 
-object ICanNameThisLiterallyAnything extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new PassthroughGenerator(69))
+object MultiplierOBJ extends App {
+  (new chisel3.stage.ChiselStage).emitVerilog(new PassthroughGenerator(64))
 }
