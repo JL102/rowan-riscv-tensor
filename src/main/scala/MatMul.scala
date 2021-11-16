45369387832afd044842000d27c0028131fdecc8
McLemoreT <mclemoret0@students.rowan.edu>
2021-11-16 18:32:39 -0500

    Fixed width refering to bitwidth instead of matrix size

--- src/main/scala/MatMul.scala
+++ src/main/scala/MatMul.scala
@@ -2,12 +2,12 @@ import chisel3._
 
 
 // Chisel Code, but pass in a parameter to set widths of ports
-class MatMul(width: Int) extends Module { 
+class MatMul(size: Int) extends Module { 
    val io = IO(new Bundle {
-      val InputMatrixA = Input(Vec(width,Vec(width,UInt(width.W))));
-      val InputMatrixB = Input(Vec(width,Vec(width,UInt(width.W)))); 
+      val InputMatrixA = Input(Vec(size,Vec(size,UInt(64.W))));
+      val InputMatrixB = Input(Vec(size,Vec(size,UInt(64.W)))); 
 
-      val out = Output(Vec(width,Vec(width,UInt(width.W))));
+      val out = Output(Vec(size,Vec(size,UInt(64.W))));
 	  });
 
 	
