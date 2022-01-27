abdeecdf451110bf8bc439b3e024a6c84b7e5f1e
JL102 <jordanlees@mailbox.org>
2022-01-27 15:27:45 -0500

    Updating

--- benchmarks/matrix1/matrix1_main.c
+++ benchmarks/matrix1/matrix1_main.c
@@ -1,16 +1,54 @@
-
+#include "rocc.h"
 #include "util.h"
-#include "dataset.h"
+
+#define DATA_SIZE 3
+
+int matrix_a[DATA_SIZE] = {
+	4,20,3
+};
+
+int matrix_b[DATA_SIZE] = {
+	31,2,11
+};
+
+// Using custom opcode 1
+
+static inline void matrix_loadA(long int matrixA) {
+	// Load memory address of A
+	// rs1 is the identifier for which item to load
+	ROCC_INSTRUCTION_SS(1, 0, &matrixA, 0b011);
+}
+
+static inline void matrix_loadB(long int matrixB) {
+	// Load memory address of B
+	ROCC_INSTRUCTION_SS(1, 1, &matrixB, 0b011);
+}
+
+static inline void multiply() {
+	// Perform multiplication
+	ROCC_INSTRUCTION_S(1, 0, 0b001);
+}
 
 int main() {
 	
 	int i;
-	int result = 0;
+	int verify_result = 0;
 	
 	for (i = 0; i < DATA_SIZE; i++) {
-		result += input_matrix_1[i] * input_matrix_2[i];
+		verify_result += matrix_a[i] * matrix_b[i];
 	}
 	
-	// printf("Calculated result: %d, verified result: %d\n", result, verify_data);
-	return verify(1, &verify_data, &result);
+	long int a = -40;
+	long int b = 634101444901;
+	
+	printf("[%d, %d, %d] * [%d, %d, %d]\n", matrix_a[0], matrix_a[1], matrix_a[2], matrix_b[0], matrix_b[1], matrix_b[2]);
+	printf("Expected: %d\n", verify_result);
+	
+	matrix_loadA(a);
+	printf("Loaded A\n");
+	
+	matrix_loadB(b);
+	printf("Loaded B\n");
+	
+	return 0;
 }
