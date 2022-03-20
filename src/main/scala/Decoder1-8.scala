21cffd97bc2738b6de1947aab375067a551e8a09
McLemoreT <mclemoret0@students.rowan.edu>
2022-03-20 18:59:54 -0400

    Brought up to parity with Decoder1-8

--- src/main/scala/Decoder1-8.scala
+++ src/main/scala/Decoder1-8.scala
@@ -6,7 +6,7 @@ import chisel3.util.{switch, is}
 
 class Decoder8_1 extends Module {
   val io = IO(new Bundle {
-    val sel = Input(Bool(3.W))
+    val sel = Input(UInt(3.W))
     val in = Input(UInt(64.W))
     val out0 = Output(UInt(64.W))
     val out1 = Output(UInt(64.W))
@@ -23,40 +23,44 @@ class Decoder8_1 extends Module {
 //Decoder passes input to designated output
 //But what happens for all the other outputs?
 //High Z?
-  switch ( sel ) {
+  switch ( io.sel ) {
 
-    is(0) {
+    is(0.U) {
       io.out0 := io.in
     }
-    is(1) {
+    is(1.U) {
       io.out1 := io.in
     }
-    is(2) {
+    is(2.U) {
       io.out2 := io.in
     }
-    is(3) {
+    is(3.U) {
       io.out3 := io.in
     }
-    is(4) {
+    is(4.U) {
       io.out4 := io.in
     }
-    is(5) {
+    is(5.U) {
       io.out5 := io.in
     }
-    is(6) {
+    is(6.U) {
       io.out6 := io.in
     }
-    is(7) {
+    is(7.U) {
       io.out7 := io.in
     }
 }
 }
+/*
 object Decoder8 {
   def apply(sel: UInt, in0: UInt, in1: UInt) = {
-    val m = Module(new Mux2)
+    val m = Module(new Decoder)
     m.io.in0 := in0
     m.io.in1 := in1
     m.io.sel := sel
     m.io.out
   }
+
+  
 }
+*/
