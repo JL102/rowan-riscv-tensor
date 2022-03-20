a22a9a1798a7427aa4f31a6210d466de8f092e81
McLemoreT <mclemoret0@students.rowan.edu>
2022-03-20 19:01:44 -0400

    Added disclaimer to bottom

--- src/main/scala/Decoder1-4.scala
+++ src/main/scala/Decoder1-4.scala
@@ -6,7 +6,7 @@ import chisel3.util.{switch, is}
 
 class Decoder4_1 extends Module {
   val io = IO(new Bundle {
-    val sel = Input(Bool(2.W))
+    val sel = Input(UInt(2.W))
     val in = Input(UInt(64.W))
     val out0 = Output(UInt(64.W))
     val out1 = Output(UInt(64.W))
@@ -18,32 +18,32 @@ class Decoder4_1 extends Module {
 //Decoder passes input to designated output
 //But what happens for all the other outputs?
 //High Z?
-  switch ( sel ) {
+  switch ( io.sel ) {
 
-    is(0) {
+    is(0.U) {
       io.out0 := io.in
     }
-    is(1) {
+    is(1.U) {
       io.out1 := io.in
     }
-    is(2) {
+    is(2.U) {
       io.out2 := io.in
     }
-    is(3) {
+    is(3.U) {
       io.out3 := io.in
     }
-    is(4) {
+    is(4.U) {
       io.out3 := io.in //This should never happen
     }
 }
 }
 
+//Not sure about this stuff down here, it compiles, but doesn't match the stuff in decoder1-8
 object Decoder4 {
-  def apply(sel: UInt, in0: UInt, in1: UInt) = {
-    val m = Module(new Mux2)
-    m.io.in0 := in0
+  def apply(sel: UInt, in: UInt) = {
+    val m = Module(new Decoder4_1)
     m.io.in := in
-    m.io.sel := sel
-    m.io.out
+
+
   }
 }
